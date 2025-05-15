c363a3d9a0a70e855b9aa3e85babf59348679cda
alverarevalo30 <alverarevalo30@gmail.com>
2025-05-15 22:27:39 +0800

    Added Logo

--- src/app/components/pages/FooterSection.jsx
+++ src/app/components/pages/FooterSection.jsx
@@ -1,10 +1,12 @@
+import Image from "next/image";
 import React from "react";
+import MyLogo from "../../../../public/Logo-gradient.svg";
 
 const FooterSection = () => {
   return (
     <footer className="footer border z-10 border-t-[var(--color-border)] border-l-transparent border-r-transparent text-white flex justify-center">
-      <div className="container p-12 flex justify-between">
-        <span>LOGO</span>
+      <div className="container p-12 flex justify-between items-center">
+        <span><Image src={MyLogo} alt="Github Icon" width={60} height={60} /></span>
         <p className="text-slate-600">All rights reserved.</p>
       </div>
     </footer>
